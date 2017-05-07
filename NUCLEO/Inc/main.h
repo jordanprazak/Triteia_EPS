e5a12ce6cadcf11202504652debe253bfe6e0a0e
jordanprazak <jordan.prazak@yahoo.com>
2017-05-07 14:44:16 -0700

    Added nucelo project and custom board project
    Cubemx project initialization for nucleo dev board and custom STM32
    target

---
+++ NUCLEO/Inc/main.h
@@ -0,0 +1,138 @@
+/**
+  ******************************************************************************
+  * File Name          : main.h
+  * Description        : This file contains the common defines of the application
+  ******************************************************************************
+  *
+  * COPYRIGHT(c) 2017 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+  /* Includes ------------------------------------------------------------------*/
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private define ------------------------------------------------------------*/
+
+#define B1_Pin GPIO_PIN_13
+#define B1_GPIO_Port GPIOC
+#define PL13_FLT_Pin GPIO_PIN_1
+#define PL13_FLT_GPIO_Port GPIOC
+#define PL10_EN_Pin GPIO_PIN_2
+#define PL10_EN_GPIO_Port GPIOC
+#define PL12_EN_Pin GPIO_PIN_3
+#define PL12_EN_GPIO_Port GPIOC
+#define BCR_OUT_EN_Pin GPIO_PIN_1
+#define BCR_OUT_EN_GPIO_Port GPIOA
+#define USART_TX_Pin GPIO_PIN_2
+#define USART_TX_GPIO_Port GPIOA
+#define USART_RX_Pin GPIO_PIN_3
+#define USART_RX_GPIO_Port GPIOA
+#define PL11_EN_Pin GPIO_PIN_4
+#define PL11_EN_GPIO_Port GPIOA
+#define LD2_Pin GPIO_PIN_5
+#define LD2_GPIO_Port GPIOA
+#define PL9_EN_Pin GPIO_PIN_6
+#define PL9_EN_GPIO_Port GPIOA
+#define PL8_EN_Pin GPIO_PIN_7
+#define PL8_EN_GPIO_Port GPIOA
+#define PL7_EN_Pin GPIO_PIN_4
+#define PL7_EN_GPIO_Port GPIOC
+#define PL6_EN_Pin GPIO_PIN_5
+#define PL6_EN_GPIO_Port GPIOC
+#define PL5_EN_Pin GPIO_PIN_0
+#define PL5_EN_GPIO_Port GPIOB
+#define PL4_EN_Pin GPIO_PIN_1
+#define PL4_EN_GPIO_Port GPIOB
+#define PL3_EN_Pin GPIO_PIN_2
+#define PL3_EN_GPIO_Port GPIOB
+#define PL2_EN_Pin GPIO_PIN_10
+#define PL2_EN_GPIO_Port GPIOB
+#define PL1_EN_Pin GPIO_PIN_11
+#define PL1_EN_GPIO_Port GPIOB
+#define PCM1_EN_Pin GPIO_PIN_12
+#define PCM1_EN_GPIO_Port GPIOB
+#define PCM2_EN_Pin GPIO_PIN_13
+#define PCM2_EN_GPIO_Port GPIOB
+#define PCM3_EN_Pin GPIO_PIN_14
+#define PCM3_EN_GPIO_Port GPIOB
+#define PCM4_EN_Pin GPIO_PIN_15
+#define PCM4_EN_GPIO_Port GPIOB
+#define PCM_IN_EN_Pin GPIO_PIN_6
+#define PCM_IN_EN_GPIO_Port GPIOC
+#define BCR1_EN_Pin GPIO_PIN_7
+#define BCR1_EN_GPIO_Port GPIOC
+#define BCR3_EN_Pin GPIO_PIN_9
+#define BCR3_EN_GPIO_Port GPIOC
+#define BCR4_EN_Pin GPIO_PIN_8
+#define BCR4_EN_GPIO_Port GPIOA
+#define TEST_LED_Pin GPIO_PIN_11
+#define TEST_LED_GPIO_Port GPIOA
+#define STATUS_LED_Pin GPIO_PIN_12
+#define STATUS_LED_GPIO_Port GPIOA
+#define TMS_Pin GPIO_PIN_13
+#define TMS_GPIO_Port GPIOA
+#define TCK_Pin GPIO_PIN_14
+#define TCK_GPIO_Port GPIOA
+#define PL1_FLT_Pin GPIO_PIN_15
+#define PL1_FLT_GPIO_Port GPIOA
+#define PL2_FLT_Pin GPIO_PIN_10
+#define PL2_FLT_GPIO_Port GPIOC
+#define PL3_FLT_Pin GPIO_PIN_11
+#define PL3_FLT_GPIO_Port GPIOC
+#define PL4_FLT_Pin GPIO_PIN_12
+#define PL4_FLT_GPIO_Port GPIOC
+#define PL5_FLT_Pin GPIO_PIN_2
+#define PL5_FLT_GPIO_Port GPIOD
+#define SWO_Pin GPIO_PIN_3
+#define SWO_GPIO_Port GPIOB
+#define PL6_FLT_Pin GPIO_PIN_4
+#define PL6_FLT_GPIO_Port GPIOB
+#define PL7_FLT_Pin GPIO_PIN_5
+#define PL7_FLT_GPIO_Port GPIOB
+#define PL8_FLT_Pin GPIO_PIN_8
+#define PL8_FLT_GPIO_Port GPIOB
+#define PL9_FLT_Pin GPIO_PIN_9
+#define PL9_FLT_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+*/ 
+
+#endif /* __MAIN_H */
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
