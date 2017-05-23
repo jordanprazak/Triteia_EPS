bd730c9afec424023d6ef36f656d8b8384b86bbb
David Tu <dctu@ucsd.edu>
2017-05-22 17:41:55 -0700

    I2C Changes
    - INA260 I2C read/write ready for testing (voltage/current/configuration
    registers)

--- NUCLEO/MDK-ARM/INA260.c
+++ NUCLEO/MDK-ARM/INA260.c
@@ -1,11 +1,14 @@
 /**
- * @Description: INA260 I2C read/write functions.
+ * @Description: I2C read/write functions to read current and bus voltage values from an INA260 chip.
  * @Author: David Tu
  * @Modified: May 15, 2017
  */
 
 #include "stm32l4xx_hal.h"
 #include "INA260.h"
+
+/* I2C1 Handle */
+static I2C_HandleTypeDef I2C1Handle = {I2C1};
 
 /* Read data via I2C */
 HAL_StatusTypeDef i2c_read( I2C_HandleTypeDef * hi2c, uint8_t device_address, uint8_t register_address, uint8_t * out_data ) {
@@ -30,17 +33,20 @@ HAL_StatusTypeDef i2c_read( I2C_HandleTypeDef * hi2c, uint8_t device_address, ui
 }
 
 /* Transmit data via I2C */
-HAL_StatusTypeDef i2c_write( I2C_HandleTypeDef * hi2c, uint8_t device_address, uint8_t register_address, uint8_t write_data ) {
+HAL_StatusTypeDef i2c_write( I2C_HandleTypeDef * hi2c, uint8_t device_address, uint8_t register_address, uint8_t write_data[], int num_bytes ) {
 	
 	/* Format data array to send */
-	uint8_t t_data[2];
+	uint8_t t_data[1 + num_bytes];
 	t_data[0] = register_address;
-	t_data[1] = write_data;
+	
+	for (int i = 0; i < num_bytes; i++ ) {
+		t_data[i + 1] = write_data[i];
+	}
 	
 	HAL_StatusTypeDef error;
 	
 	/* Send data */
-	if( ( error = HAL_I2C_Master_Transmit( hi2c, device_address, t_data, 2, 1000 ) ) != HAL_OK ) {
+	if( ( error = HAL_I2C_Master_Transmit( hi2c, device_address, t_data, 1 + num_bytes, 1000 ) ) != HAL_OK ) {
 		
 		/* Handle transmit error */
 		return error;
@@ -53,38 +59,70 @@ HAL_StatusTypeDef i2c_write( I2C_HandleTypeDef * hi2c, uint8_t device_address, u
 unsigned char getAddress( int chip_index ) {
 
   /* Map each address to an index in an array, and get the chip to read from */
-  const unsigned char address_list[] = { ADDRESS_LIST };
+  const unsigned char address_list[] = { DEVICE_ADDRESS_LIST };
   return address_list[ chip_index ];
 }
 
+/* Send configuration byte to chip (one-shot mode) */
+uint16_t sendConfig( int chip_index ) {
+	
+  /* Get the address of the chip we want to read from */
+	unsigned char device_address = getAddress( chip_index );
+	
+	/* Write buffers - send config byte to config register */
+	uint8_t register_address = CONFIG_REGISTER;
+	uint8_t write_buf[2];
+	write_buf[0] = CONFIG_MSB;
+	write_buf[1] = CONFIG_LSB;
+	
+	if( i2c_write( &I2C1Handle, device_address, register_address, write_buf, 2) != HAL_OK ) {
+		
+		/* Handle error */
+		return -1;
+	}
+	
+	return 0;
+}
+
 /* Reads in current value from specific INA260 chip on I2C bus */
-unsigned int getCurrent( int chip_index ) {
+uint16_t getCurrent( int chip_index ) {
 
   /* Get the address of the chip we want to read from */
-  unsigned char address = getAddress( chip_index );
+  unsigned char device_address = getAddress( chip_index );
+
+  /* Data buffers - get value from current register */
+  uint8_t read_buf[2];
+  uint8_t register_address = CURRENT_REGISTER;
+	
+	/* Read from register */
+	if( i2c_read( &I2C1Handle, device_address, register_address, read_buf ) != HAL_OK ) {
+		
+		/* Handle error */
+	}
 
   /* To record data from i2c bus */
-  unsigned int data = 0;
-
-  /* Read / write buffers */
-  char read_buf[2];
-  char write_buf = 0x01;
-
+  uint16_t data = (read_buf[0] << 8) | read_buf[1];
+	
 	return data;
 }
 
 /* Reads in voltage value from specific INA260 chip on I2C bus */
-unsigned int getVoltage( int chip_index ) {
+uint16_t getVoltage( int chip_index ) {
 
   /* Get the address of the chip we want to read from */
-  unsigned char address = getAddress( chip_index );
+  unsigned char device_address = getAddress( chip_index );
 
-  /* To record data from i2c bus */
-  unsigned int data = 0;
+  /* Data buffers - get value from bus voltage register */
+  uint8_t read_buf[2];
+  uint8_t register_address = VOLTAGE_REGISTER;
+	
+	/* Read from register */
+	if( i2c_read( &I2C1Handle, device_address, register_address, read_buf ) != HAL_OK ) {
+		
+		/* Handle error */
+	}
 
-  /* Read / write buffers */
-  char read_buf[2];
-  char write_buf = 0x01;
-
+	/* To record data from i2c bus */
+  uint16_t data = (read_buf[0] << 8) | read_buf[1];
 	return data;
 }
--- NUCLEO/MDK-ARM/INA260.h
+++ NUCLEO/MDK-ARM/INA260.h
@@ -8,11 +8,18 @@
 
 #include "stm32l4xx_hal.h"
 
-/* Time in microseconds for ADC shunt current and bus voltage measurements */
-#define CONVERSION_TIME_INA260 1100
-
 /* See INA260 datasheet Table 2 (p. 18) for how to hardwire them */
-#define ADDRESS_LIST 0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49
+#define DEVICE_ADDRESS_LIST 0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49
+
+#define CONFIG_REGISTER 0x00
+#define CURRENT_REGISTER 0x01
+#define VOLTAGE_REGISTER 0x02
+
+/***** Configuration Register Fields *****/
+#define RESET_BIT 0
+#define AVG_NUM_SAMPLES 4
+#define CONVERSION_TIME_INA260 1100
+#define OPERATING_MODE 0x03 // Triggered
 
 /*
   bit15 = Reset (Keep Low)
@@ -22,18 +29,18 @@
   bit5-3 = shunt voltage conversion time
   bit2-0 = operating mode
 */
-#define INA260_CONFIG_MSB 0x63 /* 0110 0011 ??? */
-#define INA260_CONFIG_LSB 0x23 /* 0010 0011 ??? */
+#define CONFIG_MSB 0x63 /* 0110 0011 */
+#define CONFIG_LSB 0x23 /* 0010 0011 */
 
 /* Generic read/write functions for INA260. Returns status value. */
 // Normal Linux i2c dev: read/write( file handler, R/W buffer, # bytes );
-HAL_StatusTypeDef i2c_read( I2C_HandleTypeDef * hi2c, uint8_t device_address, uint8_t register_address, uint8_t * out_data );
-HAL_StatusTypeDef i2c_write( I2C_HandleTypeDef * hi2c, uint8_t device_address, uint8_t register_address, uint8_t write_data );
+//HAL_StatusTypeDef i2c_read( I2C_HandleTypeDef * hi2c, uint8_t device_address, uint8_t register_address, uint8_t * out_data );
+//HAL_StatusTypeDef i2c_write( I2C_HandleTypeDef * hi2c, uint8_t device_address, uint8_t register_address, uint8_t write_data );
 
-/* Get an iv value from specific INA260 chip on I2C bus. Returns data. */
+/* Get an iv value from specific INA260 chip on I2C bus. Return data. */
 unsigned char getAddress( int chip_index );
-unsigned int getCurrent( int chip_index );
-unsigned int getVoltage( int chip_index );
-unsigned int getStatus( int chip_index );
+uint16_t getCurrent( int chip_index );
+uint16_t getVoltage( int chip_index );
+uint16_t sendConfig( int chip_index );
 
 #endif /* INA260_H */
