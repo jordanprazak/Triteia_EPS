b7253f247ed47df9e396fed82fb85840aff4c2dd
David Tu <dctu@ucsd.edu>
2017-06-04 14:40:17 -0700

    Added EPS_Status struct to header file
    And ported EPS_proto.h over, but it doesn't have the correct values

--- NUCLEO/MDK-ARM/nucleo/main.d
+++ NUCLEO/MDK-ARM/nucleo/main.d
@@ -33,3 +33,4 @@ nucleo\main.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h
 nucleo\main.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h
 nucleo\main.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h
 nucleo\main.o: ../Src/INA260.h
+nucleo\main.o: ../Src/EPS.h
